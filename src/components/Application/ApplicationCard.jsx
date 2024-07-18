686e2093eb22f41070d775f56fc63cacf79bcae6
Evarmedia <mishakmanuel@gmail.com>
2024-07-18 09:21:46 +0100

    fix: corrected button size on job and application cards

--- src/components/Application/ApplicationCard.jsx
+++ src/components/Application/ApplicationCard.jsx
@@ -69,7 +69,7 @@ const ApplicationCard = ({
 
   return (
     <div className='relative max-w-sm p-6 border rounded-lg shadow bg-gray-800 border-gray-700'>
-      <div className='h-4/5'>
+      <div className='h-4/5 overflow-hidden'>
         <NavLink to={`/jobdetails/${job_id}`}>
           <h5 className='text-lg font-semibold tracking-tight capitalize text-gray-300'>
             Job Title:{" "}<span className='text-white text-lg'>{job_title}</span>
