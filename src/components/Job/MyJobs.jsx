6a3baa0ba924066adb8e3d2da3f04e36342418f9
Evarmedia <mishakmanuel@gmail.com>
2024-07-21 19:00:09 +0100

    style: added a fixed position for the pagination on jobseeker dashboard

--- src/components/Job/MyJobs.jsx
+++ src/components/Job/MyJobs.jsx
@@ -31,26 +31,28 @@ const MyJobs = ({ searchQuery, jobType, location, currentPage, jobsPerPage, onPa
   const currentJobs = filteredJobs.slice(indexOfFirstJob, indexOfLastJob);
 
   return (
-    <div className='pt-5 flex flex-col sm:h-[690px]'>
+    <div className='pt-2 flex flex-col sm:h-[690px] relative'>
       <section className='flex-grow'>
         {user && role === "job_seeker" ? (
           <div>
-            <h1 className='text-center text-2xl font-bold mb-5'>
+            <h1 className='text-center text-2xl font-bold mb-1'>
               ALL AVAILABLE JOBS
             </h1>
             {currentJobs.length > 0 ? (
-              <div>
+              <div className="">
                 <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
                   {currentJobs.map((job) => (
                     <JobCard key={job.id} job={job} />
                   ))}
                 </div>
-                <Pagination
-                  totalJobs={filteredJobs.length}
-                  jobsPerPage={jobsPerPage}
-                  currentPage={currentPage}
-                  onPageChange={onPageChange}
-                />
+                <div className="absolute bottom-0 w-full p-2">
+                  <Pagination
+                    totalJobs={filteredJobs.length}
+                    jobsPerPage={jobsPerPage}
+                    currentPage={currentPage}
+                    onPageChange={onPageChange}
+                  />
+                </div>
               </div>
             ) : (
               <div>
